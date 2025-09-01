54ed130fe5ad48f4daa6d530afd368abea4a5e5f
karthikeya chandrapati <chandraptikarthikeya2006@gmail.com>
2025-09-01 15:18:17 +0530

    adding something

---
+++ Enum/patten1.java
@@ -0,0 +1,17 @@
+public class patten1 {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 1; i < 2*n-1; i++) {
+            int stars = i;
+            if (i > n) {
+                stars = 2 * n - i;
+            }
+            for (int j = 1; j <stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
